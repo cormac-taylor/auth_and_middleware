76aa2daa9634d72f2511be0144d3ac64a501b076
cormactaylor <cormacmtaylor@gmail.com>
2024-12-03 14:24:49 -0500

    quick save

--- helpers.js
+++ helpers.js
@@ -9,71 +9,82 @@ const MIN_QUOTE_LEN = 20;
 const MAX_QUOTE_LEN = 255;
 const VALID_ROLES = ["admin", "user"];
 
+const checkUndefined = (val) => {
+  if (typeof val === "undefined") throw "must be defined.";
+};
+
 export const validateBoolean = (bool) => {
-  if (typeof bool !== "boolean") throw "bool must be a boolean!";
+  checkUndefined(bool);
+  if (typeof bool !== "boolean") throw "must be a boolean.";
   return bool;
 };
 
 export const validateFloat = (num) => {
-  if (typeof num !== "number") throw "num must be a number!";
-  if (isNaN(num)) throw "num must have a value!";
-  if (!isFinite(num)) throw "num must be finite!";
+  checkUndefined(num);
+  if (typeof num !== "number") throw "must be a number.";
+  if (isNaN(num)) throw "must have a value.";
+  if (!isFinite(num)) throw "must be finite.";
   return num.toFixed(2);
 };
 
 export const validateInteger = (int) => {
-  if (typeof int !== "number") throw "int must be a number!";
-  if (isNaN(int)) throw "int must have a value!";
-  if (!isFinite(int)) throw "int must be finite!";
-  if (!Number.isInteger(int)) throw "int must be an integer!";
+  checkUndefined(int);
+  if (typeof int !== "number") throw "must be a number.";
+  if (isNaN(int)) throw "must have a value.";
+  if (!isFinite(int)) throw "must be finite.";
+  if (!Number.isInteger(int)) throw "must be an integer.";
   return int;
 };
 
 export const validateString = (str) => {
-  if (typeof str !== "string") throw "str must be a string!";
+  checkUndefined(str);
+  if (typeof str !== "string") throw "must be a string.";
   const res = str.trim();
-  if (!res) throw "str must not be empty!";
+  if (!res) throw "must not be empty.";
   return res;
 };
 
 export const validateStrOfLen = (str, minLen, maxLen) => {
   const res = validateString(str);
-  if (res.length < minLen) throw `str must be at least ${minLen} chars!`;
-  if (res.length > maxLen) throw `str must be at most ${maxLen} chars!`;
+  if (res.length < minLen) throw `must be at least ${minLen} chars!`;
+  if (res.length > maxLen) throw `must be at most ${maxLen} chars!`;
   return res;
 };
 
 export const validateArray = (arr) => {
-  if (!Array.isArray(arr)) throw "arr must be an array!";
+  checkUndefined(arr);
+  if (!Array.isArray(arr)) throw "must be an array.";
   return arr;
 };
 
 export const validateNonEmptyArray = (arr) => {
   const res = validateArray(arr);
-  if (res.length === 0) throw "arr must be non-empty!";
+  if (res.length === 0) throw "must be non-empty.";
   return res;
 };
 
 export const validateObject = (obj) => {
-  if (typeof obj !== "object") throw "obj must be an object!";
-  if (Array.isArray(obj)) throw "obj must not be an array!";
+  checkUndefined(obj);
+  if (typeof obj !== "object") throw "must be an object.";
+  if (Array.isArray(obj)) throw "must not be an array.";
   return obj;
 };
 
 export const validateNonEmptyObject = (obj) => {
   const res = validateObject(obj);
-  if (Object.keys(res).length === 0) throw "obj must be non-empty!";
+  if (Object.keys(res).length === 0) throw "must be non-empty.";
   return res;
 };
 
 export const validateFunction = (func) => {
-  if (typeof func !== "function") throw "func must be a function!";
+  checkUndefined(bool);
+  if (typeof func !== "function") throw "must be a function.";
   return func;
 };
 
 export const validateObjectID = (id) => {
   const res = validateString(id);
-  if (!ObjectId.isValid(res)) throw "id must be an objectID!";
+  if (!ObjectId.isValid(res)) throw "must be a mongo objectID.";
   return ObjectId.createFromHexString(res);
 };
 
@@ -82,14 +93,14 @@ export const validateName = (name) => {
   const NAME_REGEX = /^[a-zA-Z]+([ \-']{0,1}[a-zA-Z]+){0,2}[.]{0,1}$/;
 
   const res = validateStrOfLen(name, MIN_NAME_LEN, MAX_NAME_LEN);
-  if (!NAME_REGEX.test(res)) throw "name must be a valid name!";
+  if (!NAME_REGEX.test(res)) throw "must be a valid name.";
   return res;
 };
 
 export const validateUserId = (userId) => {
   const res = validateStrOfLen(userId, MIN_USERID_LEN, MAX_USERID_LEN);
   for (let c of res)
-    if ("0" <= c && c <= "9") throw "name must not contain numbers!";
+    if ("0" <= c && c <= "9") throw "must not contain numbers.";
   return res.toLowerCase();
 };
 
@@ -100,7 +111,7 @@ export const validatePassword = (password) => {
 
   const res = validateStrOfLen(password, MIN_PASSWORD_LEN, Infinity);
   if (!PASSWORD_REGEX.test(res))
-    throw "password must be a valid password! Include uppercase, digit, and special.";
+    throw "must be a valid password. Include at least one of the following: uppercase, digit, and special character.";
   return res;
 };
 
@@ -113,26 +124,25 @@ export const validateColorCode = (colorCode) => {
   const COLOR_CODE_REGEX = /^#[0-9A-Fa-f]{6}$/;
 
   const res = validateStrOfLen(colorCode, 6, 8);
-  if (!COLOR_CODE_REGEX.test(res)) throw "colorCode must be a valid color code";
+  if (!COLOR_CODE_REGEX.test(res)) throw "must be a valid hex color code.";
   return res.toUpperCase();
 };
 
 export const validateTheme = (theme) => {
   const res = validateNonEmptyObject(theme);
-  if (Object.keys(res).length !== 2) throw "theme must only contain two keys!";
+  if (Object.keys(res).length !== 2) throw "must only contain two keys.";
   try {
     res.backgroundColor = validateColorCode(res.backgroundColor);
     res.fontColor = validateColorCode(res.fontColor);
   } catch (e) {
-    throw "backgroundColor and fontColor must have hex code color values.";
+    throw "must have hex code color values.";
   }
-  if (res.backgroundColor === res.fontColor)
-    throw "backgroundColor and fontColor must not be the same!";
+  if (res.backgroundColor === res.fontColor) throw "must be different.";
   return res;
 };
 
 export const validateRole = (role) => {
   const res = validateString(role).toLowerCase();
   for (let r of VALID_ROLES) if (r === res) return res;
-  throw "role must be a valid role!";
+  throw "must be either admin or user.";
 };
--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,4 +1,13 @@
 import { Router } from "express";
+import {
+  validateColorCode,
+  validateName,
+  validatePassword,
+  validateQuote,
+  validateRole,
+  validateUserId,
+} from "../helpers";
+import { signUpUser } from "../data/users";
 const router = Router();
 
 router.route("/").get(async (req, res) => {
@@ -7,17 +16,95 @@ router.route("/").get(async (req, res) => {
 
 router
   .route("/signupuser")
-  .get(async (req, res) => {
+  .get(async (_, res) => {
     try {
-      res.render("signupuser", { pageTitle: "Sign Up" });
+      res.render("signupuser", { pageTitle: "Sign Up", hasErrors: false });
     } catch (e) {
       res.status(500).json({ error: e });
     }
   })
   .post(async (req, res) => {
-    const errors = []
-    const formData = req.body;
-    if (formData.firstName === undefined) // To Do
+    const errors = [];
+    const signupData = req.body;
+
+    try {
+      signupData.firstName = validateName(signupData.firstName);
+    } catch (e) {
+      errors.push(`firstName ${e}`);
+    }
+
+    try {
+      signupData.lastName = validateName(signupData.lastName);
+    } catch (e) {
+      errors.push(`lastName ${e}`);
+    }
+
+    try {
+      signupData.userId = validateUserId(signupData.userId);
+    } catch (e) {
+      errors.push(`userId ${e}`);
+    }
+
+    try {
+      signupData.password = validatePassword(signupData.password);
+    } catch (e) {
+      errors.push(`password ${e}`);
+    }
+
+    try {
+      signupData.confirmPassword = validatePassword(signupData.confirmPassword);
+    } catch (e) {
+      errors.push(`confirmPassword ${e}`);
+    }
+
+    try {
+      signupData.favoriteQuote = validateQuote(signupData.favoriteQuote);
+    } catch (e) {
+      errors.push(`favoriteQuote ${e}`);
+    }
+
+    let invalidColor = false;
+    try {
+      signupData.backgroundColor = validateColorCode(
+        signupData.backgroundColor
+      );
+    } catch (e) {
+      errors.push(`backgroundColor ${e}`);
+      invalidColor = true;
+    }
+
+    try {
+      signupData.fontColor = validateColorCode(signupData.fontColor);
+    } catch (e) {
+      errors.push(`fontColor ${e}`);
+      invalidColor = true;
+    }
+
+    if (!invalidColor && signupData.backgroundColor === signupData.fontColor)
+      errors.push("background and font colors must be different.");
+
+    try {
+      signupData.role = validateRole(signupData.role);
+    } catch (e) {
+      errors.push(`role ${e}`);
+    }
+
+    try {
+      if (errors.length > 0) {
+        res.render("signupuser", {
+          pageTitle: "Sign Up",
+          errors: errors,
+          hasErrors: true,
+        });
+        res.status(400);
+        return;
+      }
+      const {  } = signupData;
+      const newUser = await signUpUser(title, body, posterId, tags);
+      res.redirect(`/posts/${newPost._id}`);
+    } catch (e) {
+      res.status(500).json({ error: e });
+    }
   });
 
 router
